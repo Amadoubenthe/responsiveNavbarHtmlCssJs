c2cb40055e997211cbb51f4532f0e28f9230d913
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-07-11 16:04:59 +0000

    Add JS insctructions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 // Recuper le boutton qui a l'id 'btnToggler'
-const btnToggler = document.getElementById('btnToggler');
 
 // Recuper l'élément HTML avec la classe "navigation"
-const navigation = document.querySelector('.navigation');
 
 // Ajouter un écouteur d'événement "click" sur l'élément btnToggler, et ajouter ou retirer la classe "active" sur l'élément navigation
-
-btnToggler.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-	navigation.classList.toggle('active');
-}
